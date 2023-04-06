75f26ceb09a40f9a04a43eeef1d6d60b482832f4
Luiz Filipe <luizfilipemr17@gmail.com>
2023-04-06 18:30:55 -0300

    Adding menu controller, e o screenMethod, para alterar as telas

---
+++ src/models/controllers/MenuController.java
@@ -0,0 +1,28 @@
+package models.controllers;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class MenuController {
+
+    ScreenMethods sm = new ScreenMethods();
+
+    @FXML
+    private Button btLeaderBoard;
+
+    @FXML
+    void changeScreenLeaderboard(ActionEvent event) throws IOException {
+        sm.changeScreen("Leaderboard.fxml", event);
+    }
+
+
+    @FXML
+    void changeScreenChooseName(ActionEvent event) throws IOException {
+        sm.changeScreen("ChooseName.fxml", event);
+    }
+   
+}
---
+++ src/models/controllers/ScreenMethods.java
@@ -0,0 +1,28 @@
+package models.controllers;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class ScreenMethods {
+    
+    public void changeScreen(String path, ActionEvent event) throws IOException{
+        path = "../../panels/" + path;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(path));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        
+        stage.setResizable(false);
+        stage.setTitle("PEDRA PAPEL FACA");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
